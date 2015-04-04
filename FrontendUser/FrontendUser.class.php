7d58b92d62572d5c80f5f4192bcce07a88385cea
andre <andre@localhost.localdomain>
2015-04-04 19:23:58 +0200

    Move duplicate code to a separate php abstract class

---
+++ FrontendUser/FrontendUser.class.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Processwire 'FrontendUserLogin' module
+ * 
+ * Login and logout frontend users.
+ * 
+ * @author pwFoo
+ * @since 2015-03-10
+ * 
+ * ProcessWire 2.x
+ * Copyright (C) 2011 by Ryan Cramer
+ * Licensed under GNU/GPL v2, see LICENSE.TXT
+ *
+ * http://www.processwire.com
+ * http://www.ryancramer.com
+ */
+
+abstract class FrontendUser extends WireData implements Module {
+    protected $userObj;
+    protected $form;
+    protected $previousPage;
+
+    public function validate() {
+        $this->form->fhProcessForm();
+        return $this;
+    }
+    
+    public function render() {
+        return $this->form->render();
+    }
+    
+    public function attr($key, $value = null) {
+        if ($value === null) return $this->$key;
+        $this->$key = $value;
+    }
+    
+    protected function previousPage() {
+        $history = $this->session->getHistory();
+        $this->previousPage = end($history)['url'];
+    }
+
+    protected function buildForm($fields = null) {
+        $this->form = $this->modules->get('FormHelper')->create();
+        
+        $fields = ($fields ? $fields : array('username', 'email', 'password'));
+        foreach ($fields as $field) {
+            $this->form->add($this->$field());
+        }
+        $this->loadCssJs();         // Load CSS / JS custom or default files
+    }
+    
+    protected function loadCssJs() {
+        $this->getCssJs("{$this}.css", 'styles');
+        $this->getCssJs("{$this}.js", 'scripts');
+    }
+    
+    protected function getCssJs($file, $type) {
+        $custom = $this . '/' . $file;
+        if (file_exists($this->config->paths->templates . $custom)) {
+            $file = $this->config->urls->templates . $custom;
+        }
+        else {
+            $file = "{$this->config->urls->FrontendUserLogin}/$file";
+        }
+        $this->config->$type->add($file);
+    }
+}
